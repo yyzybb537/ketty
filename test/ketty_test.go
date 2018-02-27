31758314eb54068411648a0ec3d65836d2741b93
yuyangzi <yuyangzi@meizu.com>
2018-02-26 19:26:48 -0800

    update test code

--- test/ketty_test.go
+++ test/ketty_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	echo "github.com/yyzybb537/ketty/test/test_pb"
 	"github.com/yyzybb537/ketty"
+	kettyLog "github.com/yyzybb537/ketty/log"
 	_ "github.com/yyzybb537/ketty/protocol/grpc"
 	kettyHttp "github.com/yyzybb537/ketty/protocol/http"
 	"time"
@@ -48,14 +49,42 @@ func startClient(t *testing.T, sUrl string) {
 	t.Logf("Echo Val:%d", rsp.Val)
 }
 
-func TestGrpc(t *testing.T) {
-	urls := []string{
-		//"http://127.0.0.1",
-		"http://127.0.0.1:8091",
-		"https://127.0.0.1:3050",
-		"grpc://127.0.0.1:8090",
+var clientMap = map[string]ketty.Client{}
+
+func bStartClient(b *testing.B, sUrl string) {
+	var err error
+	client, exists := clientMap[sUrl]
+	if !exists {
+		client, err = ketty.Dial(sUrl, "")
+		if err != nil {
+			b.Fatalf("Dial error:%s", err.Error())
+		}
+		clientMap[sUrl] = client
 	}
+
+	for i := 0; i < b.N; i++ {
+		req := &echo.Req{ Val : 123 }
+		stub := echo.NewKettyEchoServiceClient(client)
+		_, err := stub.Echo(context.Background(), req)
+		if err != nil {
+			b.Fatalf("Invoke error:%+v", err)
+		}
+	}
+}
+
+var httpUrl = "http://127.0.0.1:8091"
+var httpsUrl = "https://127.0.0.1:3050"
+var grpcUrl = "grpc://127.0.0.1:8090"
+var urls = []string{
+	//"http://127.0.0.1",
+	httpUrl,
+	httpsUrl,
+	grpcUrl,
+}
+
+func TestGrpc(t *testing.T) {
 	kettyHttp.InitTLS("cert.pem", "key.pem")
+	ketty.SetLog(new(kettyLog.StdLog))
 	for _, sUrl := range urls {
 		t.Logf("Do url:%s", sUrl)
 		startServer(t, sUrl)
@@ -64,3 +93,18 @@ func TestGrpc(t *testing.T) {
 	}
 }
 
+func Benchmark_Grpc(b *testing.B) {
+	ketty.SetLog(new(kettyLog.FakeLog))
+	bStartClient(b, grpcUrl)
+}
+
+//func Benchmark_Http(b *testing.B) {
+	//ketty.SetLog(new(kettyLog.FakeLog))
+	//bStartClient(b, httpUrl)
+//}
+//
+//func Benchmark_Https(b *testing.B) {
+	//ketty.SetLog(new(kettyLog.FakeLog))
+	//bStartClient(b, httpsUrl)
+//}
+//
